c2ecef3510ac6aa65c048ef25432d9989eee9030
cissokoIdriss <cissokoi@merrimack.edu>
2025-05-06 15:36:14 -0400

    Initial Commit

---
+++ numberGuess/numberGuessApp.swift
@@ -0,0 +1,17 @@
+//
+//  numberGuessApp.swift
+//  numberGuess
+//
+//  Created by idriss cissoko on 5/6/25.
+//
+
+import SwiftUI
+
+@main
+struct numberGuessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
