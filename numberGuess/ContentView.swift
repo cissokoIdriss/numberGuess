5aacc83e15ada52e770b8381c70f63f0a533f629
cissokoIdriss <cissokoi@merrimack.edu>
2025-05-06 16:16:15 -0400

    adding app's description Text

--- numberGuess/ContentView.swift
+++ numberGuess/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Text("Welcome, ").foregroundStyle(.blue).fontWeight(.bold).font(.title).fontDesign(.monospaced).padding(11)
+            Text("I am guessRight, I use the binary search algorithm to guess the number you pick between 0 and 50, in up to 6 tries. up to the challenge ?").padding(.top, 5).padding(.bottom, 5).padding(.leading, 10).padding(.trailing, 10)
+           
+
             
         }
         
